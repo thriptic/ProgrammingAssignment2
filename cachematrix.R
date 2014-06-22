e039ccd9133ace2627443902131542b35f1b0a5c
thriptic <thriptic@gmail.com>
2014-06-22 16:53:06 -0400

    coursera submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() and cacheSolve() are employed to efficiently calculate the 
+## inverse of a defined matrix. This inverse is 'cached' so that it can be 
+## rapidly returned upon further calls of cacheSolve() without employing
+## significant CPU resources.
 
-## Write a short comment describing this function
+# makeCacheMatrix: 
+# matrix -> list
 
-makeCacheMatrix <- function(x = matrix()) {
+# Outputs 4 functions used for calculating inverse of matrix via the
+# cacheSolve function
 
+
+makeCacheMatrix <- function(mat = numeric(0)) {
+
+	manual.inverse <- numeric(0)
+	
+	set.matrix <- function(y) {  # sets matrix
+			
+		mat <<- y
+		manual.inverse <<- numeric(0) # used to clear existing cached inverted vals
+	}
+	
+	set.inverse <-function(manual) {
+	
+		manual.inverse <<- manual
+	}
+	
+	get.matrix <- function() {
+		mat
+	}
+	
+	get.inverse <- function() {
+		manual.inverse
+	}
+	
+	
+	list(setmatrix = set.matrix,
+		 setinverse = set.inverse,
+		 getmatrix = get.matrix,
+		 getinverse = get.inverse
+		)
+
+		
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+z <- makeCacheMatrix(mat) # required to avoid '$ operator on atomic vector' error
+
+
+cacheSolve <- function(r = z, ...) {
+
+## list -> matrix
+		
+## Returns the inverse value of a matrix defined in makeCacheMatrix
+## If inverse has already been calculated, returns previously calculated value.		
+		
+	inv <- r$getinverse()
+	
+	# Checks to see if inverse has already been computed.
+	if (is.null(inv)) {
+		data <- r$getmatrix() #imports matrix values from makeCacheMatrix for inversion
+		inv <- solve(data) # inverts matrix
+		r$set.inverse(inv) # sets cached value for future function calls
+	}else {  
+        message('cached value')
+	}
+	inv
+	
 }
